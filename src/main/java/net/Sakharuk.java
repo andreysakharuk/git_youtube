dc44b0ec69749ee06cf1d4d72cbf51dcb780cff0
Andrei Sakharuk <andreysakharuk@gmail.com>
2018-08-07 17:47:46 +0300

    Ad new class

--- src/main/java/net/Sakharuk.java
+++ src/main/java/net/Sakharuk.java
@@ -3,6 +3,22 @@ package net;
 /**
  * Created by Andrei_Sakharuk on 8/7/2018.
  */
-public class sakharuk {
+public class Sakharuk {
 
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Sakharuk{" +
+               "id=" + id +
+               '}';
+    }
 }
